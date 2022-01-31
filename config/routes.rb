046dd32a58c84a95af0c42f3dcbb5925417d11d4
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2022-01-31 18:33:17 +0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Code below is default route
+  # get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Code below to change the index of the route
+  root 'home#index'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
