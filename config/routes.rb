b03d6a636e2613ae5689a40e4efb23e0be7722cd
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2022-02-01 16:36:17 +0700

    Implements partial links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Code below is default route
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  get 'home/about'
   # Code below to change the index of the route
   root 'home#index'
   # Defines the root path route ("/")
